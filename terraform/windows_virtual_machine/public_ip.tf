7b0d15cde8cbde1eebf2ce301b03c7e0457431bf
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-10 15:53:33 -0500

    Adding az login step

--- terraform/windows_virtual_machine/public_ip.tf
+++ terraform/windows_virtual_machine/public_ip.tf
@@ -3,8 +3,4 @@ resource "azurerm_public_ip" "example" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   allocation_method   = "Static"
-
-  tags = {
-    environment = "Production"
-  }
 }
