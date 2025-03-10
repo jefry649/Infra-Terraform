220b64803aef726c9bd232c5d2d753d379b7259e
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-10 16:27:16 -0500

    Adding Terraform Apply step

--- dev/dev.tf
+++ dev/dev.tf
@@ -1,3 +1,19 @@
 module "rg" {
   source = "../terraform/resource-group"
 }
+
+module "kv" {
+  source = "../terraform/key_vault"
+}
+
+module "storage" {
+  source = "../terraform/storage_account" 
+}
+
+module "storage01"{
+  source = "../terraform/storage_account"
+}
+
+module "Virtual_Machine"{
+    source = "../terraform/windows_virtual_machine"
+}
--- terraform/windows_virtual_machine/nic.tf
+++ terraform/windows_virtual_machine/nic.tf
@@ -1,21 +1,21 @@
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
     name                          = "internal"
