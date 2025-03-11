9ee05388d6a079a7d64fd4779b6ca7d770ceee7d
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-10 22:53:59 -0500

    Fix VirtualMachine

--- terraform/windows_virtual_machine/public_ip.tf
+++ terraform/windows_virtual_machine/public_ip.tf
@@ -1,6 +1,6 @@
 resource "azurerm_public_ip" "example" {
   name                = var.name
-  resource_group_name = var.location
-  location            = var.rg_name
+  resource_group_name = var.rg_name
+  location            = var.location
   allocation_method   = "Static"
 }
