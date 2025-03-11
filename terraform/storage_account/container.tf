704fe6e98a0b0d49f27549403cfc448bfe5b6f19
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-10 20:29:24 -0500

    Fix StorageAccount

--- terraform/storage_account/container.tf
+++ terraform/storage_account/container.tf
@@ -1,6 +1,6 @@
-resource "azurerm_storage_container" "container" {
+resource "azurerm_storage_id" "container" {
   name                  = "micontenedor"
-  storage_account_name  = azurerm_storage_account.example.name
+  storage_account_name  = azurerm_storage_account.example.id 
   container_access_type = "private"
 }
 
