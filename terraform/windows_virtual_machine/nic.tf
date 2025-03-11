095a62d95ba391494a0e919b5921d938091eb7ca
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-10 23:01:08 -0500

    Fix VirtualMachine

--- terraform/windows_virtual_machine/nic.tf
+++ terraform/windows_virtual_machine/nic.tf
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "example" {
 resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = var.rg_name
-  virtual_network_name = var.location
+  virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
