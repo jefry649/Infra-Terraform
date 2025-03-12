9dd426863ca59851eaa5b8b25602af87c949242c
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-12 10:53:53 -0500

    Creating SQL

--- terraform/sql data/Sql.tf
+++ terraform/sql data/Sql.tf
@@ -20,7 +20,7 @@ resource "azurerm_mssql_server" "example" {
 
 resource "azurerm_mssql_database" "example" {
   name         = "appserviceplan"
-  server_id    = azurerm_mssql_server.example.example
+  server_id    = azurerm_mssql_server.example.id 
   collation    = "SQL_Latin1_General_CP1_CI_AS"
   license_type = "LicenseIncluded"
   max_size_gb  = 2
