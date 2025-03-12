69aa122e28b46f415f170f7c9102d8b3de518587
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-12 11:28:16 -0500

    Create Sql

--- terraform/sql data/Sql.tf
+++ terraform/sql data/Sql.tf
@@ -8,11 +8,6 @@ resource "azurerm_mssql_server" "example" {
   administrator_login_password = "thisIsKat11"
   minimum_tls_version          = "1.2"
 
-  azuread_administrator {
-    login_username = "AzureAD Admin"
-    object_id      = "00000000-0000-0000-0000-000000000000"
-  }
-
   tags = {
     environment = "production"
   }
