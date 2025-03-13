c923e5115e7f5e0bb6ce7bd41f4c23d622996ac1
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-13 11:27:04 -0500

    New VK

--- qa/qa.tf
+++ qa/qa.tf
@@ -5,7 +5,15 @@ module "rg" {
 
 module "kv" {
   source = "../terraform/key_vault"
-  name = "Keyvautlj"
+  name = "Keyvautlj1"
   rg_name = module.rg.rg_name
   location = module.rg.location
 } 
+
+module "azurerm_storage_account" {
+  source = "storage1j"
+  rg_mame = module.rg.rg_mame
+  location = module.rg.location 
+  account_tier = var.account_tier
+  account_replication_type = var.account_replication_type
+}
--- terraform/storage_account/variables.tf
+++ terraform/storage_account/variables.tf
@@ -7,3 +7,11 @@ variable "rg_name" {
 variable "location" {
 
 }
+
+variable "account_replication_type" {
+   
+ }
+
+variable "account_tier" {
+  
+}
