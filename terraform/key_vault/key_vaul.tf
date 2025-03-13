cbd5b2fdaee2344b08caed4577a8a84fd17dfc6c
Jefry Contreras <jefry649ct@hotmail.com>
2025-03-13 12:38:45 -0500

    create KV diagnosticsetting

--- terraform/key_vault/key_vaul.tf
+++ terraform/key_vault/key_vaul.tf
@@ -5,3 +5,17 @@ resource "azurerm_key_vault" "example" {
   sku_name            = "standard"
   tenant_id           = "72d35382-2664-4b5a-b81f-30857ebe34de" # Reemplaza con tu Tenant ID de Azure
 }
+
+resource "azurerm_monitor_diagnostic_setting" "example" {
+  name               = "example"
+  target_resource_id = azurerm_key_vault.example.id
+  storage_account_id = azurerm_storage_account.example.id
+
+  enabled_log {
+    category = "AuditEvent"
+  }
+
+  metric {
+    category = "AllMetrics"
+  }
+}
